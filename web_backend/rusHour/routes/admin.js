6b6f41a6d9eb8d646ae35835f8e2acf129bd45fa
Gohar Irfan <goharirfan94@gmail.com>
2015-03-27 02:08:34 +0500

    fixes for pushing on server.

---
+++ web_backend/rusHour/routes/admin.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('update_service_count', { title: 'RusHour' });
+});
+
+module.exports = router;
