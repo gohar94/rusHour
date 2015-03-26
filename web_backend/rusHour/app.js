82349d46822a3e4c57a29b9155cb3563ea594cc3
Gohar Irfan <goharirfan94@gmail.com>
2015-03-26 18:50:01 +0500

    user profile and dashboard.

--- web_backend/rusHour/app.js
+++ web_backend/rusHour/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var services = require('./routes/services');
+var user_dashboard = require('./routes/user_dashboard');
 var cors = require('cors')
 
 var app = express();
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/services', services);
+app.use('/user_dashboard', user_dashboard);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
