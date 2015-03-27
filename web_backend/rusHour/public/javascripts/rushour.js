5eee3d65c0712a5d691d78f4b8d67eb0904fb9f6
Mohammad Laghari <mlaghari1993@gmail.com>
2015-03-27 15:27:19 +0500

    TickerUpdate

--- web_backend/rusHour/public/javascripts/rushour.js
+++ web_backend/rusHour/public/javascripts/rushour.js
@@ -240,7 +240,16 @@ function fillTickerInitialValues() {
         var time_array = this.created_at.split("T")[1].split(":");
         var time = time_array[0]+":"+time_array[1];
         var ticker_string =  this.delta + noun + action + this.name + " at " + time;
-        $('#ticker').append($('<li>').text(ticker_string));
+        var elem_list = document.createElement('<li>');
+        var elem_fig = document.createElement('<figure>');
+        var elem_img = document.createElement('<img>');
+        elem_img.setAttribute('src', '/images/marker-green-light.png');
+        var elem_str = document.createElement('<h3>');
+        elem_str.innerHTML = ticker_string;
+        elem_fig.appendChild(elem_img);
+        elem_list.appendChild(elem_fig);
+        elem_list.appendChild(elem_list);
+        // $('#ticker-wrapper-inner').append($('<li>').text(ticker_string));
       });
     }, 
     error: function(e) { 
@@ -277,7 +286,17 @@ socket.on('update_count', function(msg){
 
 socket.on('update_ticker', function(msg){
   var today = new Date();
-  $('#ticker').append($('<li>').text(msg + " at " + today.getHours()+":"+today.getMinutes()));
+  var elem_list = document.createElement('<li>');
+  var elem_fig = document.createElement('<figure>');
+  var elem_img = document.createElement('<img>');
+  elem_img.setAttribute('src', '/images/marker-green-light.png');
+  var elem_str = document.createElement('<h3>');
+  var ticker_string = msg + " at " + today.getHours()+":"+today.getMinutes());
+  elem_str.innerHTML = ticker_string;
+  elem_fig.appendChild(elem_img);
+  elem_list.appendChild(elem_fig);
+  elem_list.appendChild(elem_list);
+  // $('#ticker-wrapper-inner').append($('<li>').text(msg + " at " + today.getHours()+":"+today.getMinutes()));
 });
 
 
@@ -337,9 +356,93 @@ function makeChart() {
   });
 }
 
+function tickerLoad() {
+    (function() {
+      function Ticker( element ) {
+        this.el = document.getElementById( element );
+        this.init();
+      }
+
+      Ticker.prototype = {
+        init: function() {
+          this.items = this.el.getElementsByTagName( "li" );
+          this.wrapper = document.getElementById( "ticker-wrapper-inner" );
+          this.totalItems = this.items.length;
+          this.timer = null;
+          this.index = 0;
+
+          this.tick();
+          this.hover();
+
+          
+        },
+
+        tick: function() {
+          var self = this;
+          self._setElementOffsets();
+
+          self.timer = setInterval(function() {
+            self.index++;
+
+            if( self.index == self.totalItems ) {
+              self.index = 0;
+            }
+
+            var item = self.items[self.index];
+            var top = item.getAttribute( "data-top" );
+            self.wrapper.style.top = "-" + top + "px";
+
+
+          }, 1500 );
+        },
+
+        hover: function() {
+          var self = this;
+          var li = self.items;
+
+          for( var i = 0; i < self.totalItems; ++i ) {
+            var item = li[i];
+            item.addEventListener( "mouseover", function() {
+              clearInterval( self.timer );
+              self.timer = null;
+
+            }, false);
+            item.addEventListener( "mouseout", function() {
+              self.tick();
+
+            }, false);
+          } 
+        },
+
+        _setElementOffsets: function() {
+          var self = this;
+          var li = self.items;
+
+          for( var i = 0; i < self.totalItems; ++i ) {
+            var item = li[i];
+            var top = item.offsetTop;
+
+            item.setAttribute( "data-top", top );
+          }
+        }
+      };
+
+      document.addEventListener( "DOMContentLoaded", function() {
+        var news = new Ticker( "ticker" );
+
+      });
+
+
+    })();
+}
+
 function onLoad() {
   makeChart();
   fillTickerInitialValues();
+  tickerLoad();
 }
 
 window.onload = onLoad();
+
+
+
