06be7532417df0d5cf17b6fd64ff31770f7510e8
Gohar Irfan <goharirfan94@gmail.com>
2015-03-28 12:36:13 +0500

    facebook stuff on page.

--- web_backend/rusHour/routes/index.js
+++ web_backend/rusHour/routes/index.js
@@ -20,7 +20,7 @@ router.get('/', function(req, res, next) {
 // route for showing the profile page
 router.get('/profile', isLoggedIn, function(req, res) {
     res.render('index', {
-        user : req.user // get the user out of session and pass to template
+        user : req.user, title: 'RusHour | ' + user.name // get the user out of session and pass to template
     });
 });
 
