a9d475c7ee0f926887e2c4ec75389a267fdc26d3
Gohar Irfan <goharirfan94@gmail.com>
2015-03-29 14:36:40 +0500

    ads. styling.

--- web_backend/rusHour/public/javascripts/rushour.js
+++ web_backend/rusHour/public/javascripts/rushour.js
@@ -425,15 +425,32 @@ function getAds() {
             console.log(category_name);
             
             var elem = document.createElement("img");
-            elem.setAttribute("width", "400px");
-            elem.setAttribute("height", "400px");
+            elem.setAttribute("width", "400");
+            elem.setAttribute("height", "400");
             elem.setAttribute("src", url);
             elem.setAttribute("alt", "Couldn't load image.");
 
             $("<li>",{ html:'<img src=' + url + ' />' }).appendTo("#rslides");
           }
           $(function() {
-            $(".rslides").responsiveSlides();
+            $(".rslides").responsiveSlides({
+              auto: true,             // Boolean: Animate automatically, true or false
+              speed: 500,            // Integer: Speed of the transition, in milliseconds
+              timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+              pager: false,           // Boolean: Show pager, true or false
+              nav: false,             // Boolean: Show navigation, true or false
+              random: false,          // Boolean: Randomize the order of the slides, true or false
+              pause: false,           // Boolean: Pause on hover, true or false
+              pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+              prevText: "Previous",   // String: Text for the "previous" button
+              nextText: "Next",       // String: Text for the "next" button
+              maxwidth: "400",           // Integer: Max-width of the slideshow, in pixels
+              navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+              manualControls: "",     // Selector: Declare custom pager navigation
+              namespace: "rslides",   // String: Change the default namespace used
+              before: function(){},   // Function: Before callback
+              after: function(){}     // Function: After callback
+            });
           });
         }, 
         error: function(e) { 
