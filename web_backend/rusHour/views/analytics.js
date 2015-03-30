f2d1168cd0f5eb62f9e05378bf342492b48cff46
Gohar Irfan <goharirfan94@gmail.com>
2015-03-30 11:36:03 +0500

    user id analytics.

--- web_backend/rusHour/views/analytics.js
+++ web_backend/rusHour/views/analytics.js
@@ -3,10 +3,13 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-61316907-1', 'auto');
 var facebook_user_id = "";
 if (document.getElementById('facebook_user_id')) {
     facebook_user_id = document.getElementById('facebook_user_id').innerHTML;
 	ga('create', 'UA-61316907-1', { 'userId': facebook_user_id });
+	console.log("sending user to analytics.");
+} else {
+	ga('create', 'UA-61316907-1', 'auto');
+	console.log("not sending user to analytics.");
 }
 ga('send', 'pageview');
