80521c044fddc108fea06bfa7699000987966298
Gohar Irfan <goharirfan94@gmail.com>
2015-04-04 23:28:29 +0500

    seed db.

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admins = [{:username => 'admin', :password => 'admin'}]
+
+admins.each do |admin|
+	Admin.create(admin)
+end
+
+service_admins = [{:username => 'yum_admin', :password => 'yum_admin', :service_id => '1'}]
+
+service_admins.each do |service_admin|
+	ServiceAdmin.create(service_admin)
+end
+
+services = 
+	[{:name => 'Yum', :address => 'MM Alam Road', :city => 'Lahore', :country => 'Pakistan', :category => 'continental', :latitude => '34', :longitude => '77', :description => 'Excellent chinese food!'}]
+
+services.each do |service|
+	Service.create(service)
+end
